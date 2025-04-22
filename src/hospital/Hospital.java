f26cac848583539c267c74d6920e2cbe53afc131
3lpython <sanelrofon2006@gmail.com>
2025-04-22 16:26:28 -0500

    Update Hospital.java

--- src/hospital/Hospital.java
+++ src/hospital/Hospital.java
@@ -1,19 +1,20 @@
 
 package hospital;
-import Backend.Logica;
+import hospital.Backend.Logica;
+import java.ulti.Scanner;
 
 public class Hospital {
 
    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Logica con = new Conexion();
+        Logica Lo = new Logica();
 
        String Nombre = sc.newline();
-       String Contraseña = sc.newline();
-       int ID_medico = sc.newline();
+       String Pass = sc.newline();
+       int IDmedico = sc.newline();
 
-       con.InsertarDatos(Nombre, contraseña, ID_medico);
+       con.InsertarDatos(Nombre, Pass, IDmedico);
     }
     
 }
