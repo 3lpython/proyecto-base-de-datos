57033ea6b2841fc9b050eb620be0fa4a8a3cc0f7
sonbaku <marcosmartinez2319@gmail.com>
2025-04-22 15:52:37 -0500

    Update Logica.java

--- src/hospital/backend/Logica.java
+++ src/hospital/backend/Logica.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package hospital.backend;
 
-/**
- *
- * @author Santiago
- */
+package hospital.backend;
+import java.sql.*;
+
 public class Logica {
+
+    public static final String url ="jdbc:mysql://localhost:3306/BDhospital";
+    public static final String user="root";
+    public static final String password="12345";
+
+    public Connection getConnection()throws SQLException{
+        return DriverManager.getConnection(url, user, password);
+    }
+    
+    public void insertarDato(String nombre){
+        
+        String sql=" INSERT INTO platos(nombre) VALUES(?)";
+        try(Connection conn=getConnection();
+                    PreparedStatement ps=conn.prepareStatement(sql);){
+            ps.setString(1,nombre);
+            ps.executeUpdate();
+            System.out.println("Dato insertado");
+            ps.close();
+        }catch (SQLException e){
+            System.out.println("Errores: " +e);
+        }
+    }
+    
+    public void mostrarDatos(){
+        
+        String sql= "SELECT * FROM platos";
+        try(Connection conn =getConnection();
+               PreparedStatement ps=conn.prepareStatement(sql);) {
+            ResultSet rs=ps.executeQuery();
+            while (rs.next()){
+                int id=rs.getInt(1);
+                String nombre=rs.getString(2);
+                
+                System.out.println("ID: "+id+ "Nombre:" +nombre);
+                
+            }rs.close();
+            
+        }catch (SQLException e){
+            System.out.println("Errores: " +e);
+        }
+    }
+
     
 }
